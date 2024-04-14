8d25dbe943343e27e1b003eb79cf342ac29be552
Hamza Hajeir <hahajeir@gmail.com>
2024-04-14 12:33:21 +0300

    Bump version 0.0.19

--- src/h4async_config.h
+++ src/h4async_config.h
@@ -24,7 +24,7 @@ SOFTWARE.
 */
 #pragma once
 #include <lwip/opt.h>
-#define H4AT_VERSION "0.0.18"
+#define H4AT_VERSION "0.0.19"
 /*
     Debug levels: 
     0 - No debug messages, no debug functions
